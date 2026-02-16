50991b5bd42397db655b6241e661a8288d223840
Dylann02 <hopkinslucka@gmail.com>
2026-02-16 23:03:23 +0300

    qq

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,16 +1,17 @@
 <?php
 
-use app\controllers\ApiExampleController;
 use app\middlewares\SecurityHeadersMiddleware;
 use app\controllers\BesoinController;
 use app\controllers\DonsController;
 use flight\Engine;
 use flight\net\Router;
-use app\middlewares\SecurityHeadersMiddleware;
 
-/** @var Router $router */
-/** @var Engine $app */
+/** 
+ * @var Router $router 
+ * @var Engine $app
+ */
 
+// This wraps all routes in the group with the SecurityHeadersMiddleware
 $router->group('', function(Router $router) use ($app) {
 	$router->get('/' ,function () use ($app){
 		$app->render('home');
@@ -19,7 +20,24 @@ $router->group('', function(Router $router) use ($app) {
 		$app->render('home');
 	});
 	$router->get('/dons' ,function () use ($app){
-		$app->render('dons');
+		$donController = new DonsController($app);
+		$dons = $donController->index();
+		$type_besoin = $donController->getAllTypeBesoin();
+		$besoins = $donController->getAllBesoin();
+		$app->render('dons', [
+			'dons' => $dons,
+			'type_besoin' => $type_besoin,
+			'besoins' => $besoins,
+			'nonce' => $app->get('csp_nonce')
+		]);
+	});
+	$router->post('/dons/create' ,function () use ($app){
+		$donController = new DonsController($app);
+		$id_besoin = (int) ($_POST['id_besoin'] ?? 0);
+		$quantite = (int) ($_POST['quantiteDon'] ?? 0);
+		$donateur = trim($_POST['donateur'] ?? '');
+		$donController->create($id_besoin, $quantite, $donateur);
+		$app->redirect('/dons');
 	});
 	$router->get('/dispatch' ,function () use ($app){
 		$app->render('dispatch');
@@ -28,6 +46,24 @@ $router->group('', function(Router $router) use ($app) {
 		$app->render('villes');
 	});
 	$router->get('/besoins' ,function () use ($app){
-		$app->render('besoins');
+		$besoin = new BesoinController();
+		$data=$besoin->getAll();
+		$ville=$besoin->getVille();
+		$typeBesoin=$besoin->getTypeBesoin();
+		$app->render('besoins' , [
+			'data' => $data , 
+			'ville' => $ville,
+			'typeBesoin' => $typeBesoin
+		]);
 	});
-}, [ SecurityHeadersMiddleware::class ]);
+	$router->get('/supprimerBesoin/@id' , function ($id) use ($app){
+		$besoin = new BesoinController();
+		$besoin->delete($id);
+		$app->redirect('/besoins');
+	});
+	$router->post('/traitementForm' , function () use ($app){
+		$besoin = new BesoinController();
+		$besoin->create();
+		$app->redirect('/besoins');
+	});
+},[ SecurityHeadersMiddleware::class ]);
