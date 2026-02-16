8be3690c2e0e2e7521e15674c3104dd1f7b81fac
Dylann02 <hopkinslucka@gmail.com>
2026-02-16 14:40:44 +0300

    suppression inutile

--- app/config/config.php
+++ app/config/config.php
@@ -46,7 +46,10 @@ if (empty($app) === true) {
 $app->path(__DIR__ . $ds . '..' . $ds . '..');
 
 // Core config variables
-$app->set('flight.base_url', '/',);           // Base URL for your app. Change if app is in a subdirectory (e.g., '/myapp/')
+// $app->set('flight.base_url', '/',); 
+$baseUrl = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/');
+$app->set('flight.base_url', $baseUrl);     
+// $app->set('flight.base_url', '/172.16.3.8/prepa_examen',);           // Base URL for your app. Change if app is in a subdirectory (e.g., '/myapp/')
 $app->set('flight.case_sensitive', false);    // Set true for case sensitive routes. Default: false
 $app->set('flight.log_errors', true);         // Log errors to file. Recommended: true in production
 $app->set('flight.handle_errors', false);     // Let Tracy handle errors if false. Set true to use Flight's error handler
@@ -65,12 +68,14 @@ return [
 	/**************************************
 	 *         Database Settings          *
 	 **************************************/
-	'database' => [
+
+
+		'database' => [
 		// MySQL Example:
-		// 'host'     => 'localhost',      // Database host (e.g., 'localhost', 'db.example.com')
-		// 'dbname'   => 'your_db_name',   // Database name (e.g., 'flightphp')
-		// 'user'     => 'your_username',  // Database user (e.g., 'root')
-		// 'password' => 'your_password',  // Database password (never commit real passwords)
+		'host'     => '127.0.0.1',      // Database host (e.g., 'localhost', 'db.example.com')
+		'dbname'   => 'BNRGC',   // Database name (e.g., 'flightphp')
+		'user'     => 'root',  // Database user (e.g., 'root')
+		'password' => '',  // Database password (never commit real passwords)
 
 		// SQLite Example:
 		// 'file_path' => __DIR__ . $ds . '..' . $ds . 'database.sqlite', // Path to SQLite file
--- app/views/welcome.php
+++
@@ -1,4 +0,0 @@
-<h1>Welcome to the FlightPHP Skeleton Example!</h1>
-<?php if(!empty($messag)) { ?>
-<h3><?=$messag?></h3>
-<?php } ?>
